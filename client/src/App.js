86cb92e7f74ff78210924bdf302b2a5541a730f2
Akif <dereakif@gmail.com>
2022-02-06 15:17:59 +0300

    create: handleFormSubmit, implement: TodoForm

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,52 @@
 import "./App.css";
 
-import { Row, Col, Card, PageHeader, Layout } from "antd";
+import { Row, Col, Card, PageHeader, Layout, notification } from "antd";
 import { Content, Header } from "antd/lib/layout/layout";
+import TodoForm from "./components/TodoForm";
+import Todos from "./apis/Todos";
+import { useState } from "react";
 
 const style = { padding: "10px" };
 
 function App() {
+  const [todoState, setTodoState] = useState({
+    loading: false,
+    data: [],
+    error: "",
+  });
+
+  const handleFormSubmit = async (values) => {
+    setTodoState((prev) => ({ ...prev, loading: true }));
+    await Todos.post("/", values)
+      .then((res) => {
+        const { data } = res;
+        if (data.todo.todo_id) {
+          setTodoState((prev) => ({
+            ...prev,
+            data: [...prev.data, data.todo],
+          }));
+          openNotification(
+            "Success",
+            `"${data.todo.title}" successfully added to the list.`
+          );
+        }
+      })
+      .catch((error) => {
+        setTodoState((prev) => ({ ...prev, error: error.message }));
+        openNotification(error.response.statusText, error.message);
+      })
+      .finally(() => {
+        setTodoState((prev) => ({ ...prev, loading: false }));
+      });
+  };
+
+  const openNotification = (title, description) => {
+    notification.open({
+      message: title,
+      description,
+    });
+  };
+
   return (
     <Layout>
       <Header>
@@ -18,7 +59,9 @@ function App() {
       <Content>
         <Row>
           <Col style={style} span={24}>
-            <Card title="Create a new todo">create todo</Card>
+            <Card title="Create a new todo">
+              <TodoForm handleFormSubmit={handleFormSubmit} />
+            </Card>
           </Col>
         </Row>
         <Row>
