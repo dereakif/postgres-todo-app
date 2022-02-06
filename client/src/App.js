a74846e0ea59f42abcfd6f5b6d7269de8b8b09ba
Akif <dereakif@gmail.com>
2022-02-06 13:23:50 +0300

    install: antd, setup: layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,34 @@
+import "./App.css";
+
+import { Row, Col, Card, PageHeader, Layout } from "antd";
+import { Content, Header } from "antd/lib/layout/layout";
+
+const style = { padding: "10px" };
+
 function App() {
-  return <div className="App">Hello world</div>;
+  return (
+    <Layout>
+      <Header>
+        <PageHeader
+          className="page-header"
+          title="Todo App"
+          subTitle="Be more productive"
+        />
+      </Header>
+      <Content>
+        <Row>
+          <Col style={style} span={24}>
+            <Card title="Create a new todo">create todo</Card>
+          </Col>
+        </Row>
+        <Row>
+          <Col style={style} span={24}>
+            <Card title="Todo List">todo list</Card>
+          </Col>
+        </Row>
+      </Content>
+    </Layout>
+  );
 }
 
 export default App;
