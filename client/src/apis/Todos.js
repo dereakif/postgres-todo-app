a12bb66f964dbead9707f0aa18e4caa6610f8701
Akif <dereakif@gmail.com>
2022-02-07 00:43:44 +0300

    done: dockerizing

--- client/src/apis/Todos.js
+++ client/src/apis/Todos.js
@@ -1,3 +1,5 @@
 import axios from "axios";
 
-export default axios.create({ baseURL: "http://localhost:3001/api/todos" });
+export default axios.create({
+  baseURL: `${process.env.REACT_APP_SERVER_URL}/api/todos`,
+});
--- server/src/db/index.js
+++ server/src/db/index.js
@@ -1,5 +1,25 @@
 const { Pool } = require("pg");
-const pool = new Pool();
+const pool = new Pool({
+  user: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
+  host: process.env.POSTGRES_HOST,
+});
+const createTable = () => {
+  let sql = `CREATE TABLE IF NOT EXISTS  todos (
+    todo_id SERIAL PRIMARY KEY,
+    title VARCHAR(50) NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    is_completed BOOL NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL
+ );`;
+  pool.query(sql).then((result) => {
+    console.log("Table created with result ->", result);
+  });
+};
+
+createTable();
 module.exports = {
   query: (text, params) => pool.query(text, params),
 };
