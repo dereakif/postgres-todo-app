8ee1921f9a11c894bf3e1abe9c4570ab22fbe75f
Akif <dereakif@gmail.com>
2022-02-06 15:09:38 +0300

    create: todo form

---
+++ client/src/components/TodoForm/index.js
@@ -0,0 +1,61 @@
+import { Form, Input, Button, Switch } from "antd";
+
+const layout = {
+  labelCol: {
+    span: 4,
+  },
+  wrapperCol: {
+    span: 16,
+  },
+};
+const tailLayout = {
+  wrapperCol: {
+    offset: 8,
+    span: 16,
+  },
+};
+const TodoForm = ({ handleFormSubmit }) => {
+  const [form] = Form.useForm();
+
+  const onReset = () => {
+    form.resetFields();
+  };
+
+  const onFinish = (values) => {
+    handleFormSubmit(values);
+    onReset();
+  };
+
+  return (
+    <Form {...layout} form={form} name="control-hooks" onFinish={onFinish}>
+      <Form.Item name="title" label="Title" rules={[{ required: true }]}>
+        <Input />
+      </Form.Item>
+      <Form.Item
+        name="description"
+        label="Description"
+        rules={[{ required: true }]}
+      >
+        <Input />
+      </Form.Item>
+      <Form.Item
+        initialValue={false}
+        label="Completed"
+        name="is_completed"
+        valuePropName="checked"
+      >
+        <Switch />
+      </Form.Item>
+      <Form.Item {...tailLayout}>
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+        <Button htmlType="button" onClick={onReset}>
+          Reset
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+};
+
+export default TodoForm;
